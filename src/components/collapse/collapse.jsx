836b73e6ad18d7ac300c1fc03e858cba83184ef0
tho.bardot@gmail.com <tho.bardot@gmail.com>
2023-11-29 16:32:56 +0100

    add chevrons and hide/unhide description

--- src/components/collapse/collapse.jsx
+++ src/components/collapse/collapse.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import ChevronUp from "../../assets/logo/chevron_up.svg";
+import ChevronDown from "../../assets/logo/chevron_down.svg";
 
 function Collapse({ title, description }) {
+  const [isExpanded, setExpanded] = useState(false);
+
+  const toggleExpand = () => {
+    setExpanded(!isExpanded);
+  };
+
   return (
-    <div className="collapse">
-      <h4 className="collapse__title">{title}</h4>
-      <p className="collapse__content">{description}</p>
+    <div className={`collapse ${isExpanded ? "expanded" : ""}`}>
+      <h4 className="collapse__title">
+        {title}{" "}
+        <span className="collapse__chevron" onClick={toggleExpand}>
+          {isExpanded ? <img src={ChevronDown} /> : <img src={ChevronUp} />}
+        </span>
+      </h4>
+      {isExpanded && <p className="collapse__content">{description}</p>}
     </div>
   );
 }
